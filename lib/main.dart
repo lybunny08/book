91abc88b48cb969c688c4980d5d5390d20c04f53
Bunny <mamitiana22.aps2a@gmail.com>
2024-01-27 13:08:13 +0300

    first de commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:book/books_splash.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+          primarySwatch: Colors.amber,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+          fontFamily: "SourceSansPro"),
+      home: BooksSplash(),
+    );
+  }
+}
